87ae54bc5e23a8017010d1969e56eb7f72d9bcaa
Tallita Silveira <47404966+TallitaSilveira1@users.noreply.github.com>
2019-06-12 17:09:03 -0300

    Update main.py

--- Heapsort/main.py
+++ Heapsort/main.py
@@ -1,81 +1,110 @@
+import random
+import timeit
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import itertools as it
-from timeit import timeit
-from random import randint
+mpl.use('Agg')
 
-def heapSort(listToSort):
-for start in range((len(listToSort)-2)//2, -1, -1):
-siftdown(listToSort, start, len(listToSort)-1)
+def geraLista(tam):
+    lista = []
+    for i in range(tam):
+        lista.append(i)
+    random.shuffle(lista)
+    return lista
 
-for end in range(len(listToSort)-1, 0, -1):
-listToSort[end], listToSort[0] = listToSort[0], listToSort[end]
-siftdown(listToSort, 0, end – 1)
-return listToSort
+def geraListaCre(tam):
+    lista = []
+    for i in range(tam):
+        lista.append(i)
+    return lista
 
-root = start
-while True:
-child = root * 2 + 1
-if child > end: break
-if child + 1 <= end and listToSort[child] < listToSort[child + 1]:
-child += 1
-if listToSort[root] < listToSort[child]:
-listToSort[root], listToSort[child] = listToSort[child], listToSort[root]
-root = child
-else:
-break
+def geraListaDec(tam):
+    lista = []
+    n = tam
+    for i in range(tam):
+        lista.append(n)
+        n -=1
+    return lista
 
-def listGenerator(lenList):
-generated = []
-for i in range(lenList):
-while len(generated) != lenList:
-n = randint(1,1*lenList)
-if n not in generated: generated.append(n)
-return generated
+def heapify(arr, n, i): 
+    largest = i  
+    l = 2 * i + 1     
+    r = 2 * i + 2     
+  
+   
+    
+    if l < n and arr[i] < arr[l]: 
+        largest = l 
+  
+    
+    if r < n and arr[largest] < arr[r]: 
+        largest = r 
+  
+  
+    if largest != i: 
+        arr[i],arr[largest] = arr[largest],arr[i] 
+  
+       
+        heapify(arr, n, largest) 
+  
 
-def drawGraph(listQt,medium,best,worst,xl = "Elementos", yl = "Tempo"):
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(111)
-ax.plot(listQt,medium, label = "Caso Medio")
-ax.plot(listQt,best, label = "Melhor Caso")
-ax.plot(listQt,worst, label="Pior Caso")
-ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
-plt.ylabel(yl)
-plt.xlabel(xl)
-plt.show()
-fig.savefig('heapSort2.png')
+def heapSort(arr): 
+    n = len(arr) 
+  
+   
+    for i in range(n, -1, -1): 
+        heapify(arr, n, i) 
+  
+   
+    for i in range(n-1, 0, -1): 
+        arr[i], arr[0] = arr[0], arr[i]  
+        heapify(arr, i, 0)
+        
+def desenhaGrafico(x, piorcaso, melhorcaso, aleatorio, name, xl = "Entradas", yl = "Tempo (s)"):
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(x, piorcaso, label = "Pior Caso")
+    ax.plot(x, melhorcaso, label="Melhor Caso")
+    ax.plot(x, aleatorio, label="Aleatório")
+    ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
+    plt.ylabel(yl)
+    plt.xlabel(xl)
+    fig.savefig(name)
 
-mediumTimes = []
-bestTimes = []
-worstTimes = []
+x = [10000, 20000, 30000, 40000, 50000]
 
-for i in length:
-mediumList = listGenerator(i)
-bestList = sorted(mediumList)
-worstList = sorted(bestList, reverse=True)
+PiorCaso = []
+MelhorCaso = []
+CasoAle = []
 
-mediumTimes.append(timeit("heapSort({})".format(mediumList),setup="from __main__ import heapSort",number=1))
-bestTimes.append(timeit("heapSort({})".format(bestList),setup="from __main__ import heapSort",number=1))
-worstTimes.append(timeit("heapSort({})".format(worstList),setup="from __main__ import heapSort",number=1))
+for i in x:
+    lista = geraListaDec(i)
+    PiorCaso.append(timeit.timeit('heapSort({})'.format(lista),setup="from __main__ import heapSort",number=1))
+    
+    lista = geraLista(i)
+    CasoAle.append(timeit.timeit('heapSort({})'.format(lista),setup="from __main__ import heapSort",number=1))
+    
+    lista = geraListaCre(i)
+    MelhorCaso.append(timeit.timeit('heapSort({})'.format(lista),setup="from __main__ import heapSort",number=1))
 
-drawGraph(length, mediumTimes, bestTimes, worstTimes)
+desenhaGrafico(x, PiorCaso, MelhorCaso, CasoAle, "graph_time.png")
 
-worstCaseExperience = list(it.permutations([1,2,3,4,5,6],6))
+lis = [1, 2, 3, 4, 5, 6]
+permut = list(it.permutations(lis,6))
+tempo = []
+listaok = []
 
-permuTimes = []
-swaps = []
+for i in permut:
+  listaok.append(list(i))
 
-for permutation in worstCaseExperience:
-listToSort = list(permutation)
-permuTimes.append(timeit("heapSort({})".format(listToSort),setup="from __main__ import heapSort",number=1))
-swaps.append(heapSort(listToSort))
+for i in range(len(listaok)):
+    tempo.append(timeit.timeit('heapSort({})'.format(listaok[i]),setup="from __main__ import heapSort",number=1))
 
-smallerTime = min(permuTimes)
-greaterTime = max(permuTimes)
+maior = tempo.index(max(tempo))
+menor = tempo.index(min(tempo))
 
-smallerTimeIndex = permuTimes.index(smallerTime)
-greaterTimeIndex = permuTimes.index(greaterTime)
-
-print("May be the worst: {}".format(worstCaseExperience[smallerTimeIndex]))
-print("May be the best: {}".format(worstCaseExperience[greaterTimeIndex]))
-print(swaps)
+print('Tempo maior:',max(tempo))
+print(permut[maior])
+print("\n")
+print('Tempo menor:',min(tempo))
+print(permut[menor])
